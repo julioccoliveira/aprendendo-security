5a7e7862293988a78e9e3e4b028e722e11235c25
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2024-04-07 19:13:06 -0300

    Moving WelcomeController to controller

--- src/main/java/com/github/aprendendosecurity/controller/WelcomeController.java
+++ src/main/java/com/github/aprendendosecurity/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.github.aprendendosecurity;
+package com.github.aprendendosecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
