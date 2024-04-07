f7a9b0dd6015a9da0186810d8578ad1d1886ecf8
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2024-04-07 20:31:35 -0300

    Setting UserService as a @Service

--- src/main/java/com/github/aprendendosecurity/service/UserService.java
+++ src/main/java/com/github/aprendendosecurity/service/UserService.java
@@ -4,7 +4,9 @@ import com.github.aprendendosecurity.model.User;
 import com.github.aprendendosecurity.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
 	@Autowired
