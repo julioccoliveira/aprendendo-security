53259ab96cd079b8afc8f865afd722526e8d797f
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2024-04-07 20:30:18 -0300

    Mapping WelcomeController

--- src/main/java/com/github/aprendendosecurity/controller/WelcomeController.java
+++ src/main/java/com/github/aprendendosecurity/controller/WelcomeController.java
@@ -1,10 +1,14 @@
 package com.github.aprendendosecurity.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.aprendendosecurity.model.User;
+import com.github.aprendendosecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/")
 public class WelcomeController {
+
 	@GetMapping
 	public String welcome() {
 		return "Welcome to this welcome";
