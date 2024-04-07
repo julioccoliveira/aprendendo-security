18eda9ce445380f442bd1682a422ed33a61ec1ec
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2024-04-07 15:45:15 -0300

    SpringBoot/Spring-Security Init - Files

---
+++ src/main/java/com/github/aprendendosecurity/AprendendoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.github.aprendendosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoSecurityApplication.class, args);
+	}
+
+}
