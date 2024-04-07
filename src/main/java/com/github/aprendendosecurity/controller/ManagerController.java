372b75ae3be62e37fae0e0335db35e811fa028a2
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2024-04-07 20:29:18 -0300

    Creating ManagerController

--- src/main/java/com/github/aprendendosecurity/controller/ManagerController.java
+++ src/main/java/com/github/aprendendosecurity/controller/ManagerController.java
@@ -1,13 +1,16 @@
 package com.github.aprendendosecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+@RequestMapping("/managers")
+public class ManagerController {
+
 	@GetMapping
-	public String welcome() {
-		return "Welcome to this welcome";
+	public String managers() {
+		return "Authorized manager";
 	}
 
 }
